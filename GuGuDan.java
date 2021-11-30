b8e3b9f6cb87b9fa55da9306c3146f77103179cd
YI <yeoniljeung@gmail.com>
2021-11-30 14:50:20 +0900

    구구단 3단

---
+++ GuGuDan.java
@@ -0,0 +1,7 @@
+public static void gugu2dan() {
+		int dan = 3, i ;
+		System.out.println("[" + dan + "단]" );
+		for ( i = 1; i <= 9; i++) {
+			System.out.println(dan + "*" + i = "=" + dan * i);
+	}
+		} 
